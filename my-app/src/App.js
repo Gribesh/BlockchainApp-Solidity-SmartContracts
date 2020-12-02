f6b7ff65887dc417eb0b82fea4d5caec8200bbbc
Gribesh <gdhakal@apexrnd.be>
2020-12-02 20:49:18 +0545

    display state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,24 @@
-import React, {Component} from 'react';
+import React, {
+  Component
+} from 'react';
 import Web3 from 'web3';
 
 class App extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
     this.web3 = new Web3(Web3.givenProvider || "ws://localhost:8546")
+    this.state = {
+      myAccountAddress: "My Contact Address"
+    }
   }
-
+  render() {
+    return (
+      <div >
+      Hello World 
+    <div>Your address is {this.state.myAccountAddress}</div>
+      </div>
+  )
+}
 }
 export default App;
